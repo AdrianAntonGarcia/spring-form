71f613ee455416ea821d8b1b9495878862fd482d
Adrián Antón <adrtler@gmail.com>
2022-02-18 11:00:25 +0100

    Implementando interceptor TiempoTranscurrido

--- src/main/java/com/bolsaideas/springboot/form/app/interceptors/TiempoTranscurridoInterceptor.java
+++ src/main/java/com/bolsaideas/springboot/form/app/interceptors/TiempoTranscurridoInterceptor.java
@@ -1,8 +1,12 @@
 package com.bolsaideas.springboot.form.app.interceptors;
 
+import java.util.Random;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
@@ -10,12 +14,21 @@ import org.springframework.web.servlet.ModelAndView;
 @Component
 public class TiempoTranscurridoInterceptor implements HandlerInterceptor {
 
+	private static final Logger logger = LoggerFactory.getLogger(TiempoTranscurridoInterceptor.class);
+
 	/**
 	 * En el handler tenemos toda la información del método que se está invocando
 	 */
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
+		logger.info("TiempoTranscurridoInterceptor: preHandler() entrando...");
+		long tiempoInicio = System.currentTimeMillis();
+		request.setAttribute("tiempoInicio", tiempoInicio);
+		Random random = new Random();
+		Integer delay = random.nextInt(500);
+		// paramos el hilo entre 0 y 499 ms random, simulamos una carga
+		Thread.sleep(delay);
 		// Si retormanos true continua la ejecución, si es false termina el proceso
 		return true;
 	}
@@ -26,7 +39,14 @@ public class TiempoTranscurridoInterceptor implements HandlerInterceptor {
 	@Override
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
 			ModelAndView modelAndView) throws Exception {
-
+		long tiempoFin = System.currentTimeMillis();
+		long tiempoInicio = (Long) request.getAttribute("tiempoInicio");
+		long tiempoTranscurrido = tiempoFin - tiempoInicio;
+		if (modelAndView != null) {
+			modelAndView.addObject("tiempoTranscurrido", tiempoTranscurrido);
+		}
+		logger.info("tiempoTranscurrido: " + tiempoTranscurrido + " milisegundos");
+		logger.info("TiempoTranscurridoInterceptor: postHandle() saliendo...");
 	}
 
 }
